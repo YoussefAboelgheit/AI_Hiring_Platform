f5fffc3794fd94d87c31578a21014e6c49f00d71
Abanoub Maqqar <abanoubmaqqar19@gmail.com>
2026-06-19 10:35:30 +0300

    chore: initial backend setup with express, mongodb, structure, env config, nodemon, morgan

---
+++ Backend/models/userModel.js
@@ -0,0 +1,6 @@
+//feel free to add more models use imort statement
+//dont use require
+// const User = require("../models/userModel");
+
+//@ desc use module not commonjs
+// import User from "../models/userModel.js"; -->true import
--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,30 @@
-const express = require('express');
+import express from 'express';
+import dotenv from "dotenv";
+import connectDB from "./config/DB.js";
+import morgan from "morgan";
+import dns from "dns";
+
+//database cluster in frankfurt
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
 const app = express();
-const port = 3001;
+app.use(morgan('dev'));
+
+
+app.use(express.json());
+dotenv.config();
+const Port = process.env.PORT || 3001;
+connectDB();
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hi Team5 MEARN Assiut');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(Port, () => {
+    console.log(`Server is running on http://localhost:${Port}`);
 });
+
